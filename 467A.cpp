d905b51e08deee12ecea19541443186eaddb22e9
Mandeep Singh <mandeepsinghshekhawat95@gmail.com>
2016-05-07 01:20:31 +0530

    467A.cpp

---
+++ 467A.cpp
@@ -0,0 +1,14 @@
+//467A
+#include<iostream>
+using namespace std;
+int main(){
+	int n,p,q;
+	int count=0;
+	cin>>n;
+	while(n--){
+		cin>>p>>q;
+		if(q-p>=2) count++;
+	}
+	cout<<count;
+	return 0;
+}
