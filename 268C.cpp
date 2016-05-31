ac8fe5f2756e5416014e64c1557656b9278b5c10
Mandeep Singh <mandeepsinghshekhawat95@gmail.com>
2016-05-31 16:58:10 +0530

    268C.cpp

---
+++ 268C.cpp
@@ -0,0 +1,13 @@
+//268C
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+	int n,m;
+	cin>>n>>m;
+	cout<<(min(n,m)+1)<<endl;
+	for(int i=0;i<=min(n,m);i++){
+		cout<<i<<" "<<min(n,m)-i<<endl;
+	}
+	return 0;
+}
